f73446f154344b2df2d6e6e8c9e3d62e463722f2
Suleymanmert <mertsuleyman0101@gmail.com>
2023-09-09 11:43:29 +0300

    9,9,2023

---
+++ src/day10_switchCase_stringManipulations/Example_02.java
@@ -0,0 +1,14 @@
+package day10_switchCase_stringManipulations;
+
+import java.util.Scanner;
+
+public class Example_02 {
+    public static void main(String[] args) {
+
+        //Soru 5- Kullanicidan gun numarasini alip
+        // hafta ici veya hafta sonu yazdirin
+
+
+    }
+
+}
