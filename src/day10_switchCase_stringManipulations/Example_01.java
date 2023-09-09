2e201cb2609f5bb7ec2907e62e905c4fa31fbc6e
Suleymanmert <mertsuleyman0101@gmail.com>
2023-09-09 11:10:57 +0300

    9,9,2023

---
+++ src/day10_switchCase_stringManipulations/Example_01.java
@@ -0,0 +1,21 @@
+package day10_switchCase_stringManipulations;
+
+import java.util.Scanner;
+
+public class Example_01 {
+    public static void main(String[] args) {
+        //Soru 4- Kullanicidan ISTQB kisaltmasindan harfin anlamini ogrenmek istedigini alin
+        // ve girilen harfin karsiligini yazdirin.
+        // I : International
+        // S : Software
+        // T : Testing
+        // Q : Qualifications
+        // B: Board
+
+
+        }
+
+
+
+    }
+}
